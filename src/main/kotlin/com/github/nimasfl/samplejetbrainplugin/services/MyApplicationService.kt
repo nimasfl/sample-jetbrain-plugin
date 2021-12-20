1a5fde8d8ec57d0e4d2be64c32937ad5bc6299ad
GitHub Action <action@github.com>
2021-12-20 08:03:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nimasfl/samplejetbrainplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nimasfl.samplejetbrainplugin.services
+
+import com.github.nimasfl.samplejetbrainplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/nimasfl/samplejetbrainplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nimasfl/samplejetbrainplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nimasfl.samplejetbrainplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nimasfl.samplejetbrainplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
